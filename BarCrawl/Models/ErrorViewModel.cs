5e96efff9351e22b03c4f83927c68d933f0bf5af
Flaka Mahmuti <flakamahmuti@gmail.com>
2019-11-25 21:14:58 -0500

    First commit

---
+++ BarCrawl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BarCrawl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
