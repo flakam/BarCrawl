589879ea12294e320c15671a6a747d93a376f495
Flaka Mahmuti <flakamahmuti@gmail.com>
2019-11-30 21:45:12 -0500

    Added a search view still api not working

--- BarCrawl/Controllers/HomeController.cs
+++ BarCrawl/Controllers/HomeController.cs
@@ -5,16 +5,85 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BarCrawl.Models;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace BarCrawl.Controllers
 {
     public class HomeController : Controller
     {
+
+        public List<YelpModel> GetBars(string location)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp($"https://api.yelp.com/v3/businesses/search+{location}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string ApiText = rd.ReadToEnd();
+
+            JToken tokens = JToken.Parse(ApiText);
+
+            List<JToken> ts = tokens["results"].ToList();
+
+            List<YelpModel> bars = new List<YelpModel>();
+
+            foreach (JToken t in ts)
+            {
+                YelpModel a = new YelpModel(t);
+                bars.Add(a);
+            }
+
+            return bars;
+        }
+        public IActionResult Result(string location)
+        {
+            List<YelpModel> bars = GetBars(location);
+            return View(bars);
+        }
+      /*  public string CallAPI(string location)
+        {
+          
+            HttpWebRequest request = WebRequest.CreateHttp($"https://api.yelp.com/v3/businesses/search+{location}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APIText = rd.ReadToEnd();
+            return APIText;
+
+        }
+        public string CallYelp(string term)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp($"https://api.yelp.com/v3/businesses/search+{term}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APIText = rd.ReadToEnd();
+            return APIText;
+
+
+        }
+        public JToken Parseyelp(string text)
+        {
+            JToken output = JToken.Parse(text);
+            return output;
+        }
+        public IActionResult Planet(string location)
+        {
+            string yelpdata = CallAPI(location);
+            JToken t = JToken.Parse(yelpdata);
+            YelpModel p= new YelpModel(t);
+            return View();
+        }*/
         public IActionResult Index()
         {
             return View();
         }
-
+        public IActionResult Search()
+        {
+            return View();
+        }
+       
+        
         public IActionResult Privacy()
         {
             return View();
---
+++ BarCrawl/Models/YelpModel.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarCrawl.Models
+{
+    public class YelpModel
+    {
+       
+
+        public List<string> Categories { get; set; }
+        public string Alias { get; set; }
+        public string Title { get; set; }
+     
+        public string Name { get; set; }
+       
+       
+        public List<string> LocationList { get; set; }
+        public string City { get; set; }
+       
+       
+        public YelpModel()
+        {
+        }
+        public YelpModel(JToken u)
+        {
+           
+            this.Alias = u["alias"].ToString();
+           
+            this.Categories = new List<string>();
+            if (u["categories"] != null)
+            {
+                foreach (JToken categories in u["categories"].ToList())
+                {
+                    this.Categories.Add(categories.ToString());
+                }
+            }
+            this.Title = u["title"].ToString();
+           
+            this.Name = u["name"].ToString();
+           
+            
+            
+            this.LocationList = new List<string>();
+            if (u["location"] != null)
+            {
+                foreach (JToken locations in u["location"].ToList())
+                {
+                    this.LocationList.Add(locations.ToString());
+                }
+            }
+            this.City = u["city"].ToString();
+                    
+           
+        }
+    }
+
+}
+
